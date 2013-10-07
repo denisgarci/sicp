b3071420eaead3ab54821df64e7ba2a13406f0eb
Denis Garcia <denisgarci@gmail.com>
2013-10-07 21:23:23 +0200

    Exercise 1-14 money change tree

---
+++ chapter1/1-14.scm
@@ -0,0 +1,28 @@
+#lang planet neil/sicp
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount (- kinds-of-coins 1))
+                 (cc (- amount (first-denomination kinds-of-coins)) kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+(count-change 100)
+;;  292
+
+(count-change 11)
+;;  4
+
+;; 11, 5 -> 10, 5 -> ... -> 0, 5 (1)
+;; 10, 4 -> 5, 4 -> 0, 4 (1)
+;; 5, 5 -> 5, 4 -> 0, 4 (1)
+;; 10, 3 -> 0, 3 (1)
