55cbd8e74e6945b9c6672aafeb28e23a141b8801
Denis Garcia <denisgarci@gmail.com>
2013-10-07 23:14:29 +0200

    Modified exercise 1-16

--- chapter1/1-16.scm
+++ chapter1/1-16.scm
@@ -10,10 +10,10 @@
 (define (fast-exp-iter b n)
  (iter 1 b n))
 
-(define (iter a b n)
- (cond ((= n 0) a)
-  ((even? n) (iter a (* b b) (/ n 2)))
-  (else (iter (* a b) b (- n 1)))))
+(define (iter acc b n)
+ (cond ((= n 0) acc)
+  ((even? n) (iter acc (* b b) (/ n 2)))
+  (else (iter (* acc b) b (- n 1)))))
 
 
 (fast-exp-iter 5 3)
