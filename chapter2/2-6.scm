b269b055a987588a9030a8960606178c5393d564
Denis Garcia <denisgarci@gmail.com>
2013-10-29 11:58:29 -0400

    Corrections from the work group

--- chapter2/2-6.scm
+++ chapter2/2-6.scm
@@ -3,17 +3,40 @@
 ;;  ex2-6
 
 
+(define (square x) (* x x))
+
+(define (inc x) (+ x 1))
 
 (define zero (lambda (f) (lambda (x) x)))
 
 (define (add-1 n)
-  (lambda (f) (lambda (x) (f ((n f) x)))))
+  (lambda (f) (lambda (x) (f ((n f) x))))) ;; n is a procedure!
 
 (define one (lambda (f) (lambda (x) (f x))))
 
 (define two (lambda (f) (lambda (x) (f (f x)))))
 
-(define (+ a b)
-    (lambda (f) (lambda (x) (a (b x)))))
+(define (add a b)
+    (lambda (f) (lambda (x) ((a f) ((b f) x)))))
 
-(+ one two)
+
+((zero inc) 7)
+;; 7
+
+((zero square) 7)
+;; 7
+
+((one inc) 7)
+;; 8
+
+((one square) 7)
+;; 49
+
+((two inc) 7)
+;; 9
+
+((two square) 2)
+;; 16
+
+(((add one two) inc) 7)
+;; 10
